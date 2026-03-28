4f8dc0e1d2dddc06e4c24fd75588be64570b1671
salim <aaqibalam290@gmail.com>
2026-03-28 14:45:38 +0530

    moving styles to .css file

--- vite-mini-project-materialUI/src/WeatherCard.jsx
+++ vite-mini-project-materialUI/src/WeatherCard.jsx
@@ -1,3 +1,4 @@
+import "./WeatherCard.css"
 const WeatherCard = ({ data }) => {
   if (!data) return <p>No data available</p>;
 
@@ -10,26 +11,24 @@ const WeatherCard = ({ data }) => {
     visibility,
   } = data;
 
-  // Convert Kelvin → Celsius
   const tempC = (main.temp - 273.15).toFixed(1);
   const feelsLikeC = (main.feels_like - 273.15).toFixed(1);
 
-  // Weather icon URL (OpenWeather)
   const icon = weather[0]?.icon;
   const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
   return (
-    <div style={styles.card}>
+    <div class  = "card">
       <h2>{name}, {sys.country}</h2>
 
-      <div style={styles.row}>
+      <div class = "row">
         <img src={iconUrl} alt="weather icon" />
         <h1>{tempC}°C</h1>
       </div>
 
-      <p style={styles.desc}>{weather[0]?.description}</p>
+      <p class = "desc">{weather[0]?.description}</p>
 
-      <div style={styles.details}>
+      <div class = "details">
         <p><strong>Feels Like:</strong> {feelsLikeC}°C</p>
         <p><strong>Humidity:</strong> {main.humidity}%</p>
         <p><strong>Pressure:</strong> {main.pressure} hPa</p>
@@ -40,31 +39,6 @@ const WeatherCard = ({ data }) => {
   );
 };
 
-const styles = {
-  card: {
-    maxWidth: "350px",
-    margin: "20px auto",
-    padding: "20px",
-    borderRadius: "12px",
-    background: "#4b5c77",
-    color: "#fff",
-    textAlign: "center",
-    boxShadow: "0 4px 10px rgba(0,0,0,0.3)",
-  },
-  row: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: "10px",
-  },
-  desc: {
-    textTransform: "capitalize",
-    marginBottom: "10px",
-  },
-  details: {
-    textAlign: "left",
-    fontSize: "14px",
-  },
-};
+
 
 export default WeatherCard;
