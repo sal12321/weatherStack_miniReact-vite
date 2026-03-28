10e3198d88d5705e1b6d9446b76c3871c7b0f400
salim <aaqibalam290@gmail.com>
2026-03-28 14:43:19 +0530

    addign error page rendering logic

--- vite-mini-project-materialUI/src/App.jsx
+++ vite-mini-project-materialUI/src/App.jsx
@@ -4,12 +4,14 @@ import { useState } from 'react';
 
 import TextField from '@mui/material/TextField';
 import WeatherCard from "./WeatherCard";
+import CityError from "./CityError";
 
 
 
 function App() {
 
   let [city, setCity] = useState("");
+  let [error, setError] = useState(null);
   const [weatherData, setWeatherData] = useState(null);
   const API_KEY = import.meta.env.VITE_API_KEY;
   
@@ -19,20 +21,40 @@ function App() {
   const API_URL=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
   let result = await fetch(API_URL);
   result = await result.json();
-  setWeatherData(result);
-  console.log(result);
+
+  if(result.cod == 200){
+
+  
+    setWeatherData(result);
+    setError(null); // no error found on response
+    // console.log(result);
+
+  }else{
+
+    // let message  = `this fucking ${city} is not found`;
+  
+  setWeatherData(null);
+    setError(result);
+
+  
+  // console.log("!=200");
+
+
+  // console.log(result);
+  }
+
   
   }
 
 
   let handleInput = (event) =>{
-  console.log(event.target.value);
+  // console.log(event.target.value);
   setCity(event.target.value);
   }
 
   let handleSubmit = (event) =>{
 
-    console.log("submit button");
+    // console.log("submit button");
     event.preventDefault();
     getWeatherData(city);
     setCity("");
@@ -66,11 +88,18 @@ function App() {
 <hr />
 
 
-  {weatherData && (
+  {weatherData&& (
         <div className="weatherBox">
           <WeatherCard data={weatherData} />
         </div>
       )}
+
+  {error&& (
+        <div className="ErrorBox" >
+          <CityError result  = {error}/>
+        </div>
+      )}
+
 
     </>
   )
